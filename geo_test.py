108358c758fea21d8d6cbd1e6cb733c99fea3b65
iaroslavrul <6969693c>
2020-05-15 02:22:10 +0300

    Добавил итерацию по sentinel. Перебирает sentinel, region и находит между ними площади полигонов вхождения

--- geo_test.py
+++ geo_test.py
@@ -1,113 +1,57 @@
 import json
 import geojson
-from functools import partial
-import pyproj
 import shapely.geometry
 import shapely.ops
 from area import area
 
-# checking to make sure they registered as polygons
-
-
-# merging the polygons - they are feature collections, containing a point, a polyline, and a polygon - I extract the polygon
-# for my purposes, they overlap, so merging produces a single polygon rather than a list of polygons
-# mergedPolygon = poly1.intersection(poly2)
-
-# using geojson module to convert from WKT back into GeoJSON format
-geojson_out = geojson.Feature(geometry=mergedPolygon, properties={})
-
-# outputting the updated geojson file - for mapping/storage in its GCS format
-with open('Merged_Polygon.json', 'w') as outfile:
-    json.dump(geojson_out.geometry, outfile, indent=3)
-outfile.close()
-
-# reprojecting the merged polygon to determine the correct area
-# it is a polygon covering much of the US, and dervied form USGS data, so using Albers Equal Area
-# project = partial(
-#     pyproj.transform,
-#     pyproj.Proj(init='epsg:4326'),
-#     pyproj.Proj(init='epsg:5070'))
-#
-# mergedPolygon_proj = shapely.ops.transform(project, mergedPolygon)
-
-with open('Merged_Polygon.json') as geojson3:
-    poly3_geojson = json.load(geojson3)
-obj = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [
-                            35.4638671875,
-                            50.43651601698633
-                        ],
-                        [
-                            34.661865234375,
-                            50.0289165635219
-                        ],
-                        [
-                            34.78271484375,
-                            48.95497369808868
-                        ],
-                        [
-                            36.37573242187499,
-                            48.378145469762444
-                        ],
-                        [
-                            37.41943359375,
-                            48.785151998043155
-                        ],
-                        [
-                            38.3038330078125,
-                            49.44312875803005
-                        ],
-                        [
-                            37.91931152343749,
-                            50.41201824668217
-                        ],
-                        [
-                            36.6009521484375,
-                            50.55532498251967
-                        ],
-                        [
-                            35.4638671875,
-                            50.43651601698633
-                        ]
-                    ]
-                ]
-            }
-        }
-    ]
-}
-print(area(poly3_geojson))
-print(area(obj['features'][0]['geometry']))
-
 
 def read_geojson(poligons_file, region_file):
-    # reading into two geojson objects, in a GCS
-    with open('big_area.json') as geojson1:
+    # reading into two geojson objects
+    with open(poligons_file) as geojson1:
         poly1_geojson = json.load(geojson1)
-    poly1 = shapely.geometry.asShape(poly1_geojson['features'][0]['geometry'])
-    with open('little_area.json') as geojson2:
+    with open(region_file) as geojson2:
         poly2_geojson = json.load(geojson2)
-    # pulling out the polygons
-    poly2 = shapely.geometry.asShape(poly2_geojson['features'][0]['geometry'])
-    print(poly1.geom_type)
-    print(poly2.geom_type)
-    return poly1, poly2
+    return poly1_geojson, poly2_geojson
+
+
+def get_intersection_area(sentinel_poligon, poly_sentinel_name, region):
+    # merging the polygons - they are feature collections, containing a point, a polyline, and a polygon - I extract the polygon
+    # for my purposes, they overlap, so merging produces a single polygon rather than a list of polygons
+    intersection_polygon = sentinel_poligon.intersection(region)
+    if intersection_polygon.is_empty:
+        pass
+    else:
+        geojson_out = geojson.Feature(geometry=intersection_polygon, properties={})
+        print(area(geojson_out.geometry))
+
+        # outputting the updated geojson file - for mapping/storage in its GCS format
+        # with open('Merged_Polygon.json', 'w') as outfile:
+        #     json.dump(geojson_out.geometry, outfile, indent=3)
+        # outfile.close()
+        # with open('Merged_Polygon.json') as geojson3:
+        #     poly3_geojson = json.load(geojson3)
+        #     print(area(poly3_geojson))
+    # using geojson module to convert from WKT back into GeoJSON format
+
+    # outputting the updated geojson file - for mapping/storage in its GCS format
+    # with open('Merged_Polygon.json', 'w') as outfile:
+    #     json.dump(geojson_out.geometry, outfile, indent=3)
+    # outfile.close()
 
 
 def iterator(sentinel_poligons, region):
-    return
+    poly_region = shapely.geometry.asShape(region['features'][0]['geometry'])
+    for poligon in sentinel_poligons['features']:
+        poly_sentinel = shapely.geometry.asShape(poligon['geometry'])
+        poly_sentinel_name = poligon['properties']["Name"]
+        get_intersection_area(poly_sentinel, poly_sentinel_name, poly_region)
+
+        # print(poly1)
 
 
 def main():
-    [poligons, region] = read_geojson("sentinel2_tiles.geojson", "Kharkiv_region.geojson")
+    [sentinel_poligons, region] = read_geojson("sentinel2_tiles.geojson", "kharkiv_region.geojson")
+    iterator(sentinel_poligons, region)
 
 
 if __name__ == '__main__':
