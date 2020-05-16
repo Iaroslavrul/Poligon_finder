5e4924c3a82ca49642cca80974a7b02a1a4c24e9
iaroslavrul <6969693c>
2020-05-16 19:07:36 +0300

    Добавил нахождение участков пересечения полигонов и обьединения их в один

--- geo_test.py
+++ geo_test.py
@@ -3,6 +3,9 @@ import geojson
 import shapely.geometry
 import shapely.ops
 from area import area
+from shapely.ops import cascaded_union
+import itertools
+from functools import reduce
 
 
 def read_geojson(poligons_file, region_file):
@@ -14,16 +17,13 @@ def read_geojson(poligons_file, region_file):
     return poly1_geojson, poly2_geojson
 
 
-def get_intersection_area(sentinel_poligon, poly_sentinel_name, region):
+def get_intersection_area(sentinel_poligon, region):
     # merging the polygons - they are feature collections, containing a point, a polyline, and a polygon - I extract the polygon
     # for my purposes, they overlap, so merging produces a single polygon rather than a list of polygons
     intersection_polygon = sentinel_poligon.intersection(region)
     if not intersection_polygon.is_empty and area(
             geojson.Feature(geometry=intersection_polygon, properties={}).geometry) > 1000:
-        poly_sentinel_dict = {}
-        poly_sentinel_dict['mame'] = poly_sentinel_name
-        poly_sentinel_dict['poly'] = geojson.Feature(geometry=intersection_polygon, properties={}).geometry
-        return poly_sentinel_dict
+        return area(geojson.Feature(geometry=intersection_polygon, properties={}).geometry)
     else:
         pass
 
@@ -42,8 +42,21 @@ def get_intersection_area(sentinel_poligon, poly_sentinel_name, region):
     # outfile.close()
 
 
-def cross_poligon(poligons_dict, region):
-    pass
+def check_cross_poligon(poligons_dict, region):
+    intersection_poligons = []
+    poly_region = shapely.geometry.asShape(region['features'][0]['geometry'])
+    for main_el in poligons_dict['features']:
+        for child_el in poligons_dict['features']:
+            main_poly = shapely.geometry.asShape(main_el['geometry'])
+            # print(main_poly)
+            child_poly = shapely.geometry.asShape(child_el['geometry'])
+            intersection_polygon = main_poly.intersection(child_poly)
+            if not intersection_polygon.is_empty and area(
+                    geojson.Feature(geometry=intersection_polygon, properties={}).geometry) < 6015317860:
+                intersection_poligons.append(intersection_polygon)
+    u = cascaded_union(intersection_poligons)
+    geojson_out = geojson.Feature(geometry=u, properties={})
+    print(geojson_out)
 
 
 def iterator(sentinel_poligons, region):
@@ -52,22 +65,21 @@ def iterator(sentinel_poligons, region):
     poligon_idx = 0
     while poligon_idx < iteration_range:
         poly_sentinel = shapely.geometry.asShape(sentinel_poligons['features'][poligon_idx]['geometry'])
-        poly_sentinel_name = sentinel_poligons['features'][poligon_idx]['properties']["Name"]
-        suitable_region = get_intersection_area(poly_sentinel, poly_sentinel_name, poly_region)
+        suitable_region = get_intersection_area(poly_sentinel, poly_region)
         if not suitable_region:
             del sentinel_poligons['features'][poligon_idx]
-            iteration_range -=1
+            iteration_range -= 1
         else:
             del sentinel_poligons['features'][poligon_idx]['properties']["description"]
             poligon_idx += 1
-
     return sentinel_poligons
 
 
 def main():
     [sentinel_poligons, region] = read_geojson("sentinel2_tiles.geojson", "kharkiv_region.geojson")
     suitable_regions = iterator(sentinel_poligons, region)
-    print(suitable_regions)
+    cross_poligon = check_cross_poligon(suitable_regions, region)
+    # print(suitable_regions)
 
 
 if __name__ == '__main__':
