c9196f0c0de52d76f47fd10357e2430e4bd9b3b2
iaroslavrul <6969693c>
2020-05-17 01:36:48 +0300

    Реализовал поиск region в зонах пересечения нескольких полигонов с полным вхождением в это пересечение region и последующим возвращением имени полигона и его геометрии

--- geo_test.py
+++ geo_test.py
@@ -42,14 +42,12 @@ def get_intersection_area(sentinel_polygon, region):
 
 
 def check_cross_polygon(polygons_dict, region):
-    intersection_polygons = []
     poly_region = shapely.geometry.asShape(region['features'][0]['geometry'])
     poly_region_default_area = area(
         geojson.Feature(geometry=poly_region, properties={}).geometry)
     for main_el in polygons_dict['features']:
         for child_el in polygons_dict['features']:
             main_poly = shapely.geometry.asShape(main_el['geometry'])
-            # print(main_poly)
             child_poly = shapely.geometry.asShape(child_el['geometry'])
             intersection_polygon = main_poly.intersection(child_poly)
             control_area = area(
@@ -60,11 +58,19 @@ def check_cross_polygon(polygons_dict, region):
                 intersection_region_area = area(geojson.Feature(geometry=intersection_region, properties={}).geometry)
                 if float("{0:.3f}".format(intersection_region_area)) == float(
                         "{0:.3f}".format(poly_region_default_area)):
-                    intersection_polygons.append(intersection_region)
+                    poly_names = []
+                    poly_names.append(main_el["properties"]["Name"])
+                    poly_names.append(child_el["properties"]["Name"])
+    result_poly_name = sorted(set(poly_names))[0]
+    for inter_poly in range(len(polygons_dict['features'])):
+        if polygons_dict['features'][inter_poly]["properties"]["Name"] != result_poly_name:
+            del polygons_dict['features'][inter_poly]
+    print(polygons_dict)
+    return polygons_dict
 
-    u = cascaded_union(intersection_polygons)
-    geojson_out = geojson.Feature(geometry=u, properties={})
-    print(geojson_out)
+    # u = cascaded_union(intersection_polygons)
+    # geojson_out = geojson.Feature(geometry=u, properties={})
+    # print(geojson_out)
 
 
 def iterator(sentinel_polygons, region):
